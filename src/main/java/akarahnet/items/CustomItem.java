d2478ba1386af3c6e6d13d180e21a28bbe94fca6
akarahdev <endisticbutitsforgit@gmail.com>
2025-04-07 19:51:33 -0400

    feat: items can now have stats

--- src/main/java/akarahnet/items/CustomItem.java
+++ src/main/java/akarahnet/items/CustomItem.java
@@ -6,6 +6,7 @@ import com.mojang.serialization.codecs.PrimitiveCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.akarah.pluginpacks.data.PluginNamespace;
 import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.ItemLore;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Material;
@@ -14,17 +15,23 @@ import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Optional;
+
 public record CustomItem(
         String name,
+        Optional<String> description,
         Material material,
-        int stars
+        int stars,
+        Optional<StatsObject> stats
 ) {
     public static PluginNamespace<CustomItem> NAMESPACE = PluginNamespace.create("citem");
 
     public static Codec<CustomItem> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             PrimitiveCodec.STRING.fieldOf("name").forGetter(CustomItem::name),
+            PrimitiveCodec.STRING.optionalFieldOf("description").forGetter(CustomItem::description),
             PrimitiveCodec.STRING.xmap(x -> Material.getMaterial(x.toUpperCase()), Material::name).fieldOf("material").forGetter(CustomItem::material),
-            PrimitiveCodec.INT.fieldOf("stars").forGetter(CustomItem::stars)
+            PrimitiveCodec.INT.fieldOf("stars").forGetter(CustomItem::stars),
+            StatsObject.CODEC.optionalFieldOf("stats").forGetter(CustomItem::stats)
     ).apply(instance, CustomItem::new));
     static Component[] STARS_LIST = {
             Component.text("✪").color(TextColor.color(255, 255, 0))
@@ -47,10 +54,16 @@ public record CustomItem(
     public ItemStack toItemStack() {
         var item = ItemStack.of(this.material());
         item.setData(DataComponentTypes.ITEM_NAME, Component.text(this.name + " ").append(STARS_LIST[this.stars - 1]));
+
+        var lore = ItemLore.lore();
+        this.stats.ifPresent(stats -> stats.addStatsToLore(lore, true));
+        item.setData(DataComponentTypes.LORE, ItemLore.lore());
         item.editMeta(meta -> {
             meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
             meta.addAttributeModifier(Attribute.ATTACK_SPEED, new AttributeModifier(Core.key("normal_combat"), 1024.0, AttributeModifier.Operation.ADD_NUMBER));
         });
+        item.setData(DataComponentTypes.LORE, lore);
+
         return item;
     }
 }
---
+++ src/main/java/akarahnet/items/StatsObject.java
@@ -0,0 +1,67 @@
+package akarahnet.items;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.PrimitiveCodec;
+import io.papermc.paper.datacomponent.item.ItemLore;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.NamespacedKey;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatsObject {
+    public static Codec<StatsObject> CODEC = Codec.unboundedMap(
+            PrimitiveCodec.STRING.xmap(NamespacedKey::fromString, NamespacedKey::asString),
+            PrimitiveCodec.DOUBLE
+    ).xmap(StatsObject::new, x -> x.stats);
+
+    public static NamespacedKey MAX_HEALTH = NamespacedKey.minecraft("max_health");
+    public static NamespacedKey MAX_MANA = NamespacedKey.minecraft("max_mana");
+    public static NamespacedKey ATTACK_DAMAGE = NamespacedKey.minecraft("attack_damage");
+    public static NamespacedKey ATTACK_SPEED = NamespacedKey.minecraft("attack_speed");
+
+    HashMap<NamespacedKey, Double> stats;
+
+    StatsObject(Map<NamespacedKey, Double> stats) {
+        this.stats = new HashMap<>(stats);
+    }
+
+    public static StatsObject of() {
+        return new StatsObject(new HashMap<>());
+    }
+
+    public void addPositiveStat(ItemLore.Builder lore, String symbol, String name, NamespacedKey key, boolean hideZeroes) {
+        if (this.stats.containsKey(key)) {
+            var value = this.stats.get(key);
+            if (value > 0.0) {
+                lore.addLine(
+                        Component.text(symbol + " " + name + ": ").color(TextColor.color(133, 133, 133))
+                                .append(Component.text("+" + value).color(TextColor.color(0, 255, 0)))
+                                .decorations(Map.of(TextDecoration.ITALIC, TextDecoration.State.FALSE))
+                );
+            } else if (value < 0.0) {
+                lore.addLine(
+                        Component.text(symbol + " " + name + ": ").color(TextColor.color(200, 200, 200))
+                                .append(Component.text(String.valueOf(value)).color(TextColor.color(255, 0, 0)))
+                                .decorations(Map.of(TextDecoration.ITALIC, TextDecoration.State.FALSE))
+                );
+            }
+        } else if (!hideZeroes) {
+            lore.addLine(
+                    Component.text(symbol + " " + name + ": ").color(TextColor.color(133, 133, 133))
+                            .append(Component.text("+0.0").color(TextColor.color(0, 255, 0)))
+                            .decorations(Map.of(TextDecoration.ITALIC, TextDecoration.State.FALSE))
+            );
+        }
+    }
+
+    public void addStatsToLore(ItemLore.Builder lore, boolean hideZeroes) {
+        System.out.println(this);
+        this.addPositiveStat(lore, "❤", "Max Health", StatsObject.MAX_HEALTH, hideZeroes);
+        this.addPositiveStat(lore, "⸎", "Max Mana", StatsObject.MAX_MANA, hideZeroes);
+        this.addPositiveStat(lore, "🗡", "Attack Damage", StatsObject.ATTACK_DAMAGE, hideZeroes);
+        this.addPositiveStat(lore, "α", "Attack Speed", StatsObject.ATTACK_SPEED, hideZeroes);
+    }
+}
