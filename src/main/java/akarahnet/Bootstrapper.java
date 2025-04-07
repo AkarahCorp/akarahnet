8350490da4dd4d6b81921b1f377420359367a65d
akarahdev <endisticbutitsforgit@gmail.com>
2025-04-07 16:27:39 -0400

    build: initialize repository

---
+++ src/main/java/akarahnet/Bootstrapper.java
@@ -0,0 +1,11 @@
+package akarahnet;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+public class Bootstrapper implements PluginBootstrap {
+    @Override
+    public void bootstrap(BootstrapContext bootstrapContext) {
+
+    }
+}
